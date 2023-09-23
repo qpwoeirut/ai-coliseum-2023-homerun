fcc81fb66d4abd4f5ef44d48d4645ec4ec36f21a
clcos <chris.h.shi@gmail.com>
2023-09-23 16:40:49 -0700

    bat logic

--- src/b_bugnav/BatterPlayer.java
+++ src/b_bugnav/BatterPlayer.java
@@ -1,19 +1,40 @@
 package b_bugnav;
-
-import aic2023.user.UnitController;
+import aic2023.user.*;
 import b_bugnav.util.FastRandom;
 
+import java.util.*;
 public class BatterPlayer {
     private final UnitController uc;
     private final FastRandom random;
-
     BatterPlayer(UnitController uc) {
         this.uc = uc;
         this.random = new FastRandom(uc.getInfo().getID());
     }
 
     void run() {
+
+
+        Team my = uc.getTeam();
         while (true) {
+            Location loc = uc.getLocation();
+            if (uc.canAct()){
+                for (int x = -1; x <=1 ; x++){
+                    for (int y = -1; y <= 1; y++){
+                        Location dir = loc.add(x,y);
+
+                        UnitInfo u = uc.senseUnitAtLocation(dir);
+                        if (u.equals(null)){
+                            continue;
+                        }
+                        //UnitType t = u.getType();
+                        else{
+                            uc.bat(loc.directionTo(dir), 3);
+                            uc.yield();
+                        }
+                    }
+                }
+
+            }
             uc.yield();
         }
     }
