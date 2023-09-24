cb97c786972e4c46dcfe78a9c882f7e2b2286ec3
ezraft <ef30369@pausd.us>
2023-09-23 22:25:35 -0700

    update BugMover to work on most maps

--- src/b_bugnav/BugMover.java
+++ src/b_bugnav/BugMover.java
@@ -7,52 +7,125 @@ public class BugMover {
     private final UnitController uc;
     private final int INF = 10000;
     private int startvar = 0;
+    private Direction prev_move = Direction.SOUTH;
+    private final int [] perm = {1,2,0,5,4,6,7,3};
     BugMover(UnitController uc) {
         this.uc = uc;
     }
 
     Direction move (Location fin)
     {
-        Direction final_move = Direction.NORTH;
         Location loc = uc.getLocation();
         int minDist = INF;
+
+        if (uc.canMove(loc.directionTo(fin)))
+        {
+            prev_move = loc.directionTo(fin);
+            return prev_move;
+        }
+
+        if (uc.canMove(prev_move))
+        {
+            return prev_move;
+        }
+
         for (int i = 0; i < 8; i++)
         {
             Direction current_dir = Direction.values()[i];
-
-            if (uc.canMove(current_dir) && loc.add(current_dir).distanceSquared(fin) < minDist)
+            if (current_dir.isEqual(prev_move))
             {
-                final_move = current_dir;
-                minDist = loc.add(current_dir).distanceSquared(fin);
+                Direction next_dir = Direction.values()[(i + 2) % 8];
+                Direction prev_dir = Direction.values()[(i + 6) % 8];
+                if (uc.canMove(next_dir))
+                {
+                    prev_move = next_dir;
+                    return prev_move;
+                } else if (uc.canMove(prev_dir))
+                {
+                    prev_move = prev_dir;
+                    return prev_move;
+                }
             }
         }
-
-        return final_move;
+        prev_move = Direction.NORTHEAST;
+        return prev_move;
     }
 
     Direction move_to_object (MapObject object)
     {
-        uc.println("startvar: " + startvar);
+        uc.println("startvar: " + startvar + " " + Direction.values()[startvar]);
         Location loc = uc.getLocation();
         Location[] objects = uc.senseObjects(object, INF);
+        int current_index = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            Direction current_dir = Direction.values()[i];
+            if (current_dir.isEqual(prev_move))
+            {
+                current_index = i;
+            }
+        }
 
+        if ((int)(uc.getRandomDouble() * 30) == 0 && uc.canMove(Direction.values()[(current_index + 1) % 8]))
+        {
+            prev_move = Direction.values()[(current_index + 1) % 8];
+            return prev_move;
+        }
+
+        if ((int)(uc.getRandomDouble() * 30) == 0 && uc.canMove(Direction.values()[(current_index + 5) % 8]))
+        {
+            prev_move = Direction.values()[(current_index + 5) % 8];
+            return prev_move;
+        }
+        if (uc.canMove(prev_move))
+        {
+            return prev_move;
+        }
         if (objects.length != 0)
         {
             //report existence of object
         }
 
-        float vision_range = uc.getType().getStat(UnitStat.VISION_RANGE);
+        Direction next_dir = Direction.values()[(current_index + 2) % 8];
+        Direction prev_dir = Direction.values()[(current_index + 6) % 8];
+
+        if ((int)(uc.getRandomDouble() * 2) == 0)
+        {
+            if (uc.canMove(next_dir))
+            {
+                prev_move = next_dir;
+                return prev_move;
+            } else if (uc.canMove(prev_dir))
+            {
+                prev_move = prev_dir;
+                return prev_move;
+            }
+        } else
+        {
+            if (uc.canMove(prev_dir))
+            {
+                prev_move = prev_dir;
+                return prev_move;
+            } else if (uc.canMove(next_dir))
+            {
+                prev_move = next_dir;
+                return prev_move;
+            }
+        }
+
+//        float vision_range = uc.getType().getStat(UnitStat.VISION_RANGE);
         for (int i = startvar; i < 8; i++)
         {
             Direction current_dir = Direction.values()[i];
-            Location end_goal = loc.add(3 * (current_dir.dx), 3 * (current_dir.dy));
-            if (!uc.isOutOfMap(end_goal))
+            if (!uc.canMove(current_dir))
             {
-                return move(end_goal);
+                prev_move = current_dir;
+                return prev_move;
             }
-            startvar = (startvar + 1) % 8;
-            uc.println("got here");
         }
-        return Direction.NORTH;
+        Direction final_dir = Direction.NORTHEAST;
+        uc.println("here again");
+        prev_move = final_dir;
+        return final_dir;
     }
 }
