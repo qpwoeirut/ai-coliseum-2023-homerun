a06d35c96d332045676a8c90a739bd93d8cbb9b5
ezraft <ef30369@pausd.us>
2023-09-23 18:53:25 -0700

    Add new maps and update BugMover to incorporate getting information

--- src/b_bugnav/BugMover.java
+++ src/b_bugnav/BugMover.java
@@ -6,6 +6,7 @@ public class BugMover {
 
     private final UnitController uc;
     private final int INF = 10000;
+    private int startvar = 0;
     BugMover(UnitController uc) {
         this.uc = uc;
     }
@@ -31,20 +32,26 @@ public class BugMover {
 
     Direction move_to_object (MapObject object)
     {
+        uc.println("startvar: " + startvar);
         Location loc = uc.getLocation();
-        Location[] bases = uc.senseObjects(object, INF);
-        if (bases.length == 0)
+        Location[] objects = uc.senseObjects(object, INF);
+
+        if (objects.length != 0)
         {
-            float vision_range = uc.getType().getStat(UnitStat.VISION_RANGE);
-            for (int i = 0; i < 8; i++)
+            //report existence of object
+        }
+
+        float vision_range = uc.getType().getStat(UnitStat.VISION_RANGE);
+        for (int i = startvar; i < 8; i++)
+        {
+            Direction current_dir = Direction.values()[i];
+            Location end_goal = loc.add(3 * (current_dir.dx), 3 * (current_dir.dy));
+            if (!uc.isOutOfMap(end_goal))
             {
-                Direction current_dir = Direction.values()[i];
-                Location end_goal = loc.add((int)vision_range * (current_dir.dx), (int)vision_range * (current_dir.dy));
-                if (!uc.isOutOfMap(end_goal))
-                {
-                    return move(end_goal);
-                }
+                return move(end_goal);
             }
+            startvar = (startvar + 1) % 8;
+            uc.println("got here");
         }
         return Direction.NORTH;
     }
--- src/b_bugnav/CatcherPlayer.java
+++ src/b_bugnav/CatcherPlayer.java
@@ -1,16 +1,19 @@
 package b_bugnav;
 
+import aic2023.user.MapObject;
 import aic2023.user.UnitController;
 
 public class CatcherPlayer {
     private final UnitController uc;
-
+    private final BugMover bg;
     CatcherPlayer(UnitController uc) {
         this.uc = uc;
+        this.bg = new BugMover(uc);
     }
 
     void run() {
         while (true) {
+            uc.move(bg.move_to_object(MapObject.BASE));
             uc.yield();
         }
     }
