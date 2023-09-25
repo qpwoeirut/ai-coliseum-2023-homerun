488832e174b2cad7718a9164eb2a621945dfddf5
qpwoeirut <stanleyzhong8@gmail.com>
2023-09-25 13:43:56 -0700

    Fix issue with Location object comparison

--- src/b_bugnav/BugMover.java
+++ src/b_bugnav/BugMover.java
@@ -68,7 +68,8 @@ public class BugMover {
         }
 
         Location loc = uc.getLocation();
-        if (target != fin) {
+        if (!fin.isEqual(target)) {
+            uc.println("new target received");
             init(fin);
         }
         visited[prev_move.ordinal()].put(Util.packLoc(loc), 1);
@@ -124,6 +125,7 @@ public class BugMover {
 
         // return ZERO to avoid making invalid moves
         if (++stuckCount >= STUCK_THRESHOLD) {
+            uc.println("Got stuck on attempt " + attempts + ", " + stuckCount + " times");
             init(fin);
             ++attempts;  // TODO: once this is too high, assume the target is unreachable
             return move(fin);
@@ -161,64 +163,5 @@ public class BugMover {
             dir = leftTurn ? dir.rotateLeft() : dir.rotateRight();
         }
         return null;
-    }
-
-    Direction move_to_objects() {
-//        uc.println("startvar: " + startvar + " " + Direction.values()[startvar]);
-        Location loc = uc.getLocation();
-        int current_index = 0;
-        for (int i = 0; i < 8; i++) {
-            Direction current_dir = Direction.values()[i];
-            if (current_dir.isEqual(prev_move)) {
-                current_index = i;
-            }
-        }
-
-        if ((int) (uc.getRandomDouble() * 30) == 0 && uc.canMove(Direction.values()[(current_index + 1) % 8])) {
-            prev_move = Direction.values()[(current_index + 1) % 8];
-            return prev_move;
-        }
-
-        if ((int) (uc.getRandomDouble() * 30) == 0 && uc.canMove(Direction.values()[(current_index + 5) % 8])) {
-            prev_move = Direction.values()[(current_index + 5) % 8];
-            return prev_move;
-        }
-        if (uc.canMove(prev_move)) {
-            return prev_move;
-        }
-
-        Direction next_dir = Direction.values()[(current_index + 2) % 8];
-        Direction prev_dir = Direction.values()[(current_index + 6) % 8];
-
-        if ((int) (uc.getRandomDouble() * 2) == 0) {
-            if (uc.canMove(next_dir)) {
-                prev_move = next_dir;
-                return prev_move;
-            } else if (uc.canMove(prev_dir)) {
-                prev_move = prev_dir;
-                return prev_move;
-            }
-        } else {
-            if (uc.canMove(prev_dir)) {
-                prev_move = prev_dir;
-                return prev_move;
-            } else if (uc.canMove(next_dir)) {
-                prev_move = next_dir;
-                return prev_move;
-            }
-        }
-
-//        float vision_range = uc.getType().getStat(UnitStat.VISION_RANGE);
-        for (int i = 0; i < 8; i++) {
-            Direction current_dir = Direction.values()[i];
-            if (!uc.canMove(current_dir)) {
-                prev_move = current_dir;
-                return prev_move;
-            }
-        }
-        Direction final_dir = Direction.NORTHEAST;
-//        uc.println("here again");
-        prev_move = final_dir;
-        return final_dir;
     }
 }
