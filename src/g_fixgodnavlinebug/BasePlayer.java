7163d4e7cde70c455be0c62ea4d7e4702edad2b6
qpwoeirut <stanleyzhong8@gmail.com>
2023-09-29 10:21:21 -0700

    Add g_fixgodnavlinebug, mark f_adjustmicrofixgodnav as excluded

---
+++ src/g_fixgodnavlinebug/BasePlayer.java
@@ -0,0 +1,68 @@
+package g_fixgodnavlinebug;
+
+import aic2023.user.*;
+import g_fixgodnavlinebug.util.Communications;
+
+abstract public class BasePlayer {
+    protected final UnitController uc;
+    protected final Communications comms;
+    protected final BugMover bg;
+    protected final float VISION;
+    protected final int URGENCY_FACTOR;
+
+    BasePlayer(UnitController uc) {
+        this.uc = uc;
+        this.comms = new Communications(uc);
+        this.bg = new BugMover(uc);
+
+        VISION = uc.getType().getStat(UnitStat.VISION_RANGE);
+        if (uc.getType() == UnitType.BATTER) {
+            URGENCY_FACTOR = 1;
+        } else if (uc.getType() == UnitType.CATCHER) {
+            URGENCY_FACTOR = 2;
+        } else if (uc.getType() == UnitType.PITCHER) {
+            URGENCY_FACTOR = 10;
+        } else if (uc.getType() == UnitType.HQ) {
+            URGENCY_FACTOR = 6;
+        } else URGENCY_FACTOR = 1;
+    }
+
+    protected Location[] senseAndReportBases() {
+        Location[] bases = uc.senseObjects(MapObject.BASE, VISION);
+        comms.reportNewBases(bases);
+        return bases;
+    }
+
+    protected Location[] senseAndReportStadiums() {
+        Location[] stadiums = uc.senseObjects(MapObject.STADIUM, VISION);
+        comms.reportNewStadiums(stadiums);
+        return stadiums;
+    }
+
+    protected UnitInfo[] senseAndReportEnemies() {
+        UnitInfo[] enemies = uc.senseUnits(VISION, uc.getOpponent());
+        comms.reportEnemySightings(enemies, URGENCY_FACTOR);
+        return enemies;
+    }
+
+    protected void senseAndReportGrassIfNecessary() {
+        if (uc.getEnergyLeft() >= 1500 && !comms.grassAlreadySensedAtLocation()) {
+            comms.reportNewGrassAtEndOfTurn(uc.senseObjects(MapObject.GRASS, VISION));
+        }
+    }
+
+    protected void endTurn() {
+        final int currentRound = uc.getRound();
+        senseAndReportGrassIfNecessary();
+        if (uc.getRound() == currentRound) comms.useRemainingBytecode();
+        if (uc.getRound() == currentRound) uc.yield();
+    }
+
+    protected void debug(String message) {
+        if (uc.getRound() <= 200) uc.println(message);
+    }
+
+    protected void debugBytecode(String message) {
+        debug(message + " " + uc.getEnergyUsed());
+    }
+}
---
+++ src/g_fixgodnavlinebug/util/IntHashMap.java
@@ -0,0 +1,59 @@
+package g_fixgodnavlinebug.util;
+
+import java.util.Arrays;
+
+// from https://github.com/TheK098/Battlecode2023/blob/main/src/qp1_13_tuning/utilities/IntHashMap.java
+
+/**
+ * An int -> int hashmap
+ * 0 is the default value for nonexistent keys
+ */
+public class IntHashMap {
+    private final int n;
+    //    private int size;
+    private final int[] ind;
+    private final int[][] keys, values;
+
+    public IntHashMap(int bucketCount) {
+        n = bucketCount;
+        ind = new int[bucketCount];
+        keys = new int[bucketCount][4];
+        values = new int[bucketCount][4];
+    }
+
+    public void put(int key, int value) {
+        int hash = key % n;
+        int cind = ind[hash];
+        int[] ckeys = keys[hash];
+        int[] cvalues = values[hash];
+        for (int i = cind; i --> 0;) {
+            if (ckeys[i] == key) {
+                cvalues[i] = value;
+                return;
+            }
+        }
+        if (cind == ckeys.length) {
+            keys[hash] = Arrays.copyOf(ckeys, ckeys.length * 2);
+            values[hash] = Arrays.copyOf(cvalues, ckeys.length * 2);
+            keys[hash][cind] = key;
+            values[hash][cind] = value;
+        } else {
+            ckeys[cind] = key;
+            cvalues[cind] = value;
+        }
+        ++ind[hash];
+//        size++;
+    }
+
+    public int get(int key) {
+        int hash = key % n;
+        int[] ckeys = keys[hash];
+        int[] cvalues = values[hash];
+        for (int i = ind[hash]; i --> 0;) {
+            if (ckeys[i] == key) {
+                return cvalues[i];
+            }
+        }
+        return 0;
+    }
+}
