860fe829174414587de4361ea23b2b201acb1d7b
qpwoeirut <stanleyzhong8@gmail.com>
2023-10-08 14:31:48 -0700

    Add submitted o_adjustmicro.zip, mark version O as excluded, add new p_final version

---
+++ src/p_final/BugMover.java
@@ -0,0 +1,191 @@
+package p_final;
+
+import aic2023.user.Direction;
+import aic2023.user.Location;
+import aic2023.user.UnitController;
+import p_final.util.IntHashMap;
+import p_final.util.Util;
+
+public class BugMover {
+
+    private final UnitController uc;
+    private final int INF = 10000;
+    private int stage = 0;
+    private Location target = null;
+    private Direction prev_move = Direction.ZERO;
+    private int minDist;
+
+    private boolean followingWall = false;
+
+    private final boolean clockwise;  // decide randomly whether this bug will turn left or right
+    private final IntHashMap[] visited = new IntHashMap[Direction.values().length];
+    private final int BUCKET_COUNT = 8;
+    private final int STUCK_THRESHOLD = 5;  // after this many failed moves, restart the algorithm
+    private int stuckCount = 0;
+    private int attempts = 0;
+
+    BugMover(UnitController uc) {
+        this.uc = uc;
+        this.clockwise = uc.getInfo().getID() % 2 == 0;
+    }
+
+    void init(Location fin) {
+//        uc.println("Setting target to " + fin);
+        target = fin;
+        stage = 0;
+
+        prev_move = Direction.ZERO;
+        stuckCount = 0;
+        attempts = 0;
+        minDist = INF;
+
+        for (int i = Direction.values().length - 1; i >= 0; --i) {
+            visited[i] = new IntHashMap(BUCKET_COUNT);
+        }
+
+        followingWall = false;
+    }
+    private void useDirection(Direction dir) {
+        prev_move = dir;
+        stuckCount = 0;
+        attempts = 0;
+    }
+
+    /**
+     * Recommends best direction to move to the provided location. If at the location, returns ZERO.
+     * Uses bug 0 algorithm until it hits a loop, then uses bug 1 algorithm.
+     * If the location is unreachable, will return Direction.ZERO
+     * Bug0 can be tested with the Flooded map. Bug1 can be tested on Comb.
+     * @param fin Location to move to
+     * @return direction to move in
+     */
+    public Direction move(Location fin) {
+        // TODO: add an @param nullIfUnreachable whether to return null or Direction.ZERO if the location is unreachable
+
+        if (!uc.canMove()) return Direction.ZERO;
+        if (!uc.canMove(Direction.NORTH) && !uc.canMove(Direction.NORTHEAST) &&
+                !uc.canMove(Direction.EAST) && !uc.canMove(Direction.SOUTHEAST) &&
+                !uc.canMove(Direction.SOUTH) && !uc.canMove(Direction.SOUTHWEST) &&
+                !uc.canMove(Direction.WEST) && !uc.canMove(Direction.NORTHWEST)) {
+            return Direction.ZERO;
+        }
+
+        Location loc = uc.getLocation();
+        if (!fin.isEqual(target)) {
+//            uc.println("new target received");
+            init(fin);
+        }
+        visited[prev_move.ordinal()].put(Util.packLoc(loc), 1);
+
+        if (stage == 0) {  // try bug0 first
+//            uc.println("stage 0, fin " + fin);
+            final Direction dir = bug0(fin);
+            if (dir != null) {
+                useDirection(dir);
+                return dir;
+            }
+
+            stage = 1;
+            for (int i = Direction.values().length - 1; i >= 0; --i) {
+                visited[i] = new IntHashMap(BUCKET_COUNT);
+            }
+        }
+        if (stage == 1) {  // bug0 has failed >:( try bug1
+//            uc.println("stage 1, fin " + fin);
+            minDist = Math.min(minDist, loc.distanceSquared(fin));
+            final Direction dir = wallFollow();
+            if (dir != null) {
+                useDirection(dir);
+                return dir;
+            }
+
+            stage = 2;
+            for (int i = Direction.values().length - 1; i >= 0; --i) {
+                visited[i] = new IntHashMap(BUCKET_COUNT);
+            }
+        }
+        if (stage == 2) {
+//            uc.println("stage 2, fin, minDist " + fin + ", " + minDist);
+            if (loc.distanceSquared(fin) <= minDist) {
+                stage = 0;
+                prev_move = loc.directionTo(fin);
+                for (int i = Direction.values().length - 1; i >= 0; --i) {
+                    visited[i] = new IntHashMap(BUCKET_COUNT);
+                }
+
+                final Direction dir = move(fin);
+                if (dir != null) {
+                    useDirection(dir);
+                    return dir;
+                }
+            }
+            final Direction dir = wallFollow();
+            if (dir != null) {
+                useDirection(dir);
+                return dir;
+            }
+        }
+
+        // return ZERO to avoid making invalid moves
+        if (++stuckCount >= STUCK_THRESHOLD) {
+//            uc.println("Got stuck on attempt " + attempts + ", " + stuckCount + " times");
+            init(fin);
+            ++attempts;  // TODO: once this is too high, assume the target is unreachable
+            return move(fin);
+        }
+        return Direction.ZERO;
+    }
+
+    public Direction moveUsingBug0(Location fin) {
+        if (!uc.canMove()) return Direction.ZERO;
+        if (!uc.canMove(Direction.NORTH) && !uc.canMove(Direction.NORTHEAST) &&
+                !uc.canMove(Direction.EAST) && !uc.canMove(Direction.SOUTHEAST) &&
+                !uc.canMove(Direction.SOUTH) && !uc.canMove(Direction.SOUTHWEST) &&
+                !uc.canMove(Direction.WEST) && !uc.canMove(Direction.NORTHWEST)) {
+            return Direction.ZERO;
+        }
+
+        Location loc = uc.getLocation();
+        if (!fin.isEqual(target)) {
+//            uc.println("new target received");
+            init(fin);
+        }
+        if (visited[prev_move.ordinal()].get(Util.packLoc(loc)) == 1) return null;  // this line is necessary since we sometimes swap away from bug0
+        visited[prev_move.ordinal()].put(Util.packLoc(loc), 1);
+        return bug0(fin);
+    }
+
+    private Direction bug0(Location fin) {
+        final Location loc = uc.getLocation();
+        if (uc.canMove(loc.directionTo(fin))) {
+            prev_move = loc.directionTo(fin);
+            followingWall = false;
+
+            if (visited[prev_move.ordinal()].get(Util.packLoc(loc.add(prev_move))) == 1) {
+                return null;
+            } else {
+                return prev_move;
+            }
+        }
+        return wallFollow();
+    }
+
+    private Direction wallFollow() {
+        final Location loc = uc.getLocation();
+        Direction dir = followingWall ^ clockwise ? prev_move.rotateLeft().rotateLeft() : prev_move.rotateRight().rotateRight();
+        followingWall = true;
+        // initial heading should be reversed since we're cutting back inside
+        for (int i = 7; i >= 0; --i) {
+            if (uc.canMove(dir)) {
+                if (visited[dir.ordinal()].get(Util.packLoc(loc.add(dir))) == 1) {
+                    return null;
+                } else {
+                    prev_move = dir;
+                    return dir;
+                }
+            }
+            dir = clockwise ? dir.rotateLeft() : dir.rotateRight();
+        }
+        return null;
+    }
+}
---
+++ src/p_final/CatcherPlayer.java
@@ -0,0 +1,115 @@
+package p_final;
+
+import aic2023.user.*;
+import p_final.util.Util;
+
+public class CatcherPlayer extends BasePlayer {
+    int scoutTimer = 30;
+    boolean reachedFocalPoint = false;
+    Location target = null;
+
+    Direction scoutDir = Direction.values()[uc.getInfo().getID() % 8];
+    final int SCOUT_TIMER = 100;
+    Location spawn;
+
+    CatcherPlayer(UnitController uc) {
+        super(uc);
+        this.spawn = uc.getLocation();
+    }
+
+    void run() {
+        while (true) {
+//            debugBytecode("start turn");
+            comms.checkIn();
+            senseAndReportBases();
+            senseAndReportStadiums();
+//            debugBytecode("after reporting objects");
+
+            final UnitInfo[] nearbyEnemies = uc.senseUnits(REACHABLE_VISION, uc.getOpponent());
+            final int directionOkay = calculateOkayDirections(nearbyEnemies);
+//            debug("directionOkay = " + directionOkay);
+            final UnitInfo nearestEnemyBatter = Util.getNearestChebyshev(uc.getLocation(), nearbyEnemies, UnitType.BATTER);
+//            debug("nearest enemy batter is " + nearestEnemyBatter);
+            if (nearestEnemyBatter != null && ((directionOkay >> Direction.ZERO.ordinal()) & 1) == 0) {
+//                debug("moving away from " + nearestEnemyBatter.getLocation());
+                Util.tryMoveInDirection(uc, nearestEnemyBatter.getLocation().directionTo(uc.getLocation()));
+            }
+//            debugBytecode("after run away");
+
+//            debugBytecode("before move");
+            if (uc.canMove()) {
+                if (target == null ||
+                        uc.getLocation().distanceSquared(target) <= 4 ||  // this cutoff is intentionally lower than the cutoff in comms
+                        (uc.getLocation().distanceSquared(target) <= VISION &&
+                                (uc.isOutOfMap(target) || uc.senseObjectAtLocation(target, false) == MapObject.WATER))) {
+                    target = comms.popNearestScoutingQueue();
+                    reachedFocalPoint = false;
+                    if (target != null) scoutTimer = (int)(Util.movementDistance(uc.getLocation(), target) * 2);
+                } else {
+                    comms.updateScoutingQueue();
+                }
+//                debug("target = " + target + ", scoutTimer = " + scoutTimer);
+                if (target != null) {
+                    scoutTarget(directionOkay);
+                } else {
+                    scoutRandomLine(directionOkay);
+                }
+            }
+
+            endTurn();
+        }
+    }
+
+    void scoutTarget(int directionOkay) {
+//        debug("lb dist = " + comms.lowerBoundDistance(uc.getLocation(), target) + ", reachedFP = " + reachedFocalPoint);
+        Direction dir = null;
+        if (!reachedFocalPoint) {
+            final int mapIdx = comms.findBestDistanceMapIdx(uc.getLocation(), target);
+//            debug("mapIdx = " + mapIdx + ", distFp = " + comms.distanceFromFocalPoint(mapIdx, target));
+            if (comms.lowerBoundDistanceGreaterThan(uc.getLocation(), target, comms.distanceFromFocalPoint(mapIdx, target))) {
+                dir = comms.directionViaFocalPoint(target, directionOkay);
+            }
+        }
+//        debug("dir from comms = " + dir);
+        if (dir == Direction.ZERO) {
+            dir = null;
+            reachedFocalPoint = true;
+        }
+        if (dir == null) {
+            dir = bg.moveUsingBug0(target);  // don't bother with bug1. if bug0 fails, just change targets
+        }
+        if (dir == null) target = null;
+
+        if (dir != null && dir != Direction.ZERO && uc.canMove(dir) && ((directionOkay >> dir.ordinal()) & 1) > 0) {
+            uc.move(dir);
+        } else {
+            Util.tryMoveInOkayDirection(uc, Direction.values()[(int)(uc.getRandomDouble() * 8)], directionOkay);
+        }
+
+        if (--scoutTimer <= 0) {
+            target = null;
+        }
+    }
+
+    void scoutRandomLine(int directionOkay) {
+        final UnitInfo[] nearbyEnemies = uc.senseUnits(REACHABLE_VISION, uc.getOpponent());
+        final UnitInfo nearestEnemyBatter = Util.getNearestChebyshev(uc.getLocation(), nearbyEnemies, UnitType.BATTER);
+//            debug("nearest enemy batter is " + nearestEnemyBatter);
+        if (nearestEnemyBatter != null && ((directionOkay >> Direction.ZERO.ordinal()) & 1) == 0) {
+            Util.tryMoveInDirection(uc, nearestEnemyBatter.getLocation().directionTo(uc.getLocation()));
+        }
+
+        if (uc.isOutOfMap(uc.getLocation().add(scoutDir.dx * 4, scoutDir.dy * 4)) || --scoutTimer == 0) {
+            int shift = (int)(uc.getRandomDouble() * 6) + 1;
+            if (shift >= 4) ++shift;
+            scoutDir = Direction.values()[(scoutDir.ordinal() + shift) % 8];
+            scoutTimer = SCOUT_TIMER;
+        }
+        final Direction dir = bg.move(spawn.add(scoutDir.dx * 55, scoutDir.dy * 55));
+        if (dir != null && dir != Direction.ZERO) {
+            Util.tryMoveInOkayDirection(uc, dir, directionOkay);
+        } else {
+            Util.tryMoveInOkayDirection(uc, scoutDir, directionOkay);
+        }
+    }
+}
